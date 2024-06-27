28708f7646fa93913e169ae1ec5d328833dd3537
Macka <martina.bobokova@gmail.com>
2024-06-27 21:44:38 +0200

    add url endpoint to data

--- main.py
+++ main.py
@@ -12,14 +12,28 @@ def home():
     return render_template("home.html", stations_info=stations_info.to_html())
 
 
-@app.route("/api/v1/<station>/<date>")
-def about(station, date):
+@app.route("/api/v1/<station>")
+def full_station(station):
     filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
-    return {"station": station,
-            "date": date,
-            "temperature": temperature}
+    df['   TG'] = df['   TG'].squeeze() / 10
+    return df.to_dict(orient="records")
+
+
+@app.route("/api/v1/<station>/<date>")
+def day(station, date):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    if len(date) == 4:
+        df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+        df["    DATE"] = df["    DATE"].astype(str)
+        result = df[df["    DATE"].str.startswith(str(date))].to_dict(orient="records")
+        return result
+    else:
+        df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+        temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+        return {"station": station,
+                "date": date,
+                "temperature": temperature}
 
 
 if __name__ == '__main__':
